a7dc6f4c3ff291f277e3db80e46b6d0c35197dab
Harishvemulakonda <harishvemulakonda1992@gmail.com>
2021-11-15 23:17:56 +0530

    initial coomm

---
+++ src/main/java/ramtech/hhjar/App.java
@@ -0,0 +1,13 @@
+package ramtech.hhjar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
