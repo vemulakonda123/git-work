2db302662e3d6b6fae5eec3ba7b3d9e9f2a86961
Harishvemulakonda <50296141+Harishvemulakonda@users.noreply.github.com>
2021-11-22 23:47:51 +0530

    second

--- src/main/java/ramtech/hhjar/App.java
+++ src/main/java/ramtech/hhjar/App.java
@@ -12,7 +12,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );  
-            System.out.println("hi");
+            System.out.println("hi everone");
 
     }
 }
